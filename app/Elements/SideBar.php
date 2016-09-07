78d3cc36846b171c0166dfbcaca99c22b2587ab2
KiyaDoig <kiya.doig@gmail.com>
2016-09-07 15:56:19 +1000

    Implements initial layout

---
+++ app/Elements/SideBar.php
@@ -0,0 +1,19 @@
+<div class="col-md-3" id="side-bar">
+    <div class="row" id="main-header-left">
+        <h3>Ruthless</h3>
+    </div>
+    <div class="row">
+        <div id="user-panel">
+            <img src="../Images/ic_account_circle_white_48dp_1x.png" class="img-circle" alt="User Image">
+            <span id="user-name">GetUserName</span>
+        </div>
+    </div>
+    <div class="row separator"></div>
+    <div class="row">
+        <!-- Navigation -->
+        <nav role="navigation">
+            <?php include 'Nav.php' ?>
+        </nav>
+    </div>
+
+</div>
