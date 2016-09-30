16974ca85796e03ebd5063cf2e6658b4b33bc742
BerettaFTW <slpri8@student.monash.edu>
2016-09-30 17:57:07 +1000

    Fixed login page. Files removed while chaning the header.

--- app/login-page/Login.php
+++ app/login-page/Login.php
@@ -1,13 +1,3 @@
-<?php
-//create MDS constants
-define("MONASH_DIR", "ldap.monash.edu.au");
-define("MONASH_FILTER","o=Monash University, c=au");
-
-ob_start();
-session_start();
-$_SESSION["page"] = "Login";
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,8 +7,8 @@ $_SESSION["page"] = "Login";
     <meta http-equiv="x-ua-compatible" content="ie=edge">
 
     <title>
-        Ruthless Real Estate
-    </title>
+Ruthless Real Estate
+</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/css/bootstrap.min.css" integrity="sha384-2hfp1SzUoho7/TsGGGDaFdsuuDL0LX2hnUp6VkX3CUQ2K4K+xjboZdsXyp4oUHZj" crossorigin="anonymous">
@@ -26,10 +16,16 @@ $_SESSION["page"] = "Login";
     <link rel="stylesheet" type="text/css" href="../app.css" />
 </head>
 
+<?php
+//create MDS constants
+define("MONASH_DIR", "ldap.monash.edu.au");
+define("MONASH_FILTER","o=Monash University, c=au");
+?>
+
 <body>
-<div class="row" id="main-header">
-    <?php include '../Elements/MainHeader.php' ?>
-</div>
+    <div class="row" id="main-header">
+        <?php include '../Elements/MainHeader.php' ?>
+    </div>
 
 <div class="row" id="main-area">
     <?php include '../Elements/SideBar.php' ?>
@@ -41,118 +37,116 @@ $_SESSION["page"] = "Login";
                 <!-- write content here -->
                 <h1>Login Page</h1>
 
+                <?php
+                if(empty($_POST["uname"]))
+                {
+                ?>
                 <div class="container">
-                    <?php
-                    if(empty($_POST["uname"]))
-                    {
-                        ?>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <form method="post" Action="">
-                                    <h3 class="form-signin-heading" id="log">Authcate Sign In</h3>
-
-                                    <script>
-                                        var  login = '<?php echo $_SESSION["login"]; ?>';
-                                        if(login){
-                                            document.getElementById("log").innerHTML = "Welcome, you are logged in";
-                                        }
-                                        else{
-                                            document.getElementById("log").innerHTML = "Please log in";
-                                            <?php $_SESSION["old"] = false; ?>
-
-                                        }
-                                    </script>
-
-                                    <div class="form-group row">
-                                        <label for="uname" class="col-xs-2 col-form-label">Username</label>
-                                        <div class="col-xs-10">
-                                            <input class="form-control" type="text" value="" id="uname" name="uname">
-                                        </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <form method="post" Action="">
+                                <center>Please log in using your Authcate Details
+                                </center>
+                                <div class="form-group row">
+                                    <label for="uname" class="col-xs-2 col-form-label">Username</label>
+                                    <div class="col-xs-10">
+                                        <input class="form-control" type="text" value="" id="uname" name="uname">
                                     </div>
-                                    <div class="form-group row">
-                                        <label for="pword" class="col-xs-2 col-form-label">Password</label>
-                                        <div class="col-xs-10">
-                                            <input class="form-control" type="password" value="" id="pword" name="pword">
-                                        </div>
+                                </div>
+
+                                <div class="form-group row">
+                                    <label for="pword" class="col-xs-2 col-form-label">Password</label>
+                                    <div class="col-xs-10">
+                                        <input class="form-control" type="password" value="" id="pword" name="pword">
                                     </div>
-                                    <button type="submit" class="btn btn-primary">Log in</button>
-                                </form>
-                            </div>
-                        </div>
-                        <?php
-                    }
-                    else
-                    {
-                        $LDAPconn=@ldap_connect(MONASH_DIR);
-                        if($LDAPconn)
-                        {
-                            $LDAPsearch=@ldap_search($LDAPconn,MONASH_FILTER,
-                                "uid=".$_POST["uname"]);
-                            if($LDAPsearch)
-                            {
-                                $LDAPinfo =
-                                    @ldap_first_entry($LDAPconn,$LDAPsearch);
-                                if($LDAPinfo)
-                                {
-                                    $LDAPresult=
-                                        @ldap_bind($LDAPconn,
-                                            ldap_get_dn($LDAPconn, $LDAPinfo),
-                                            $_POST["pword"]);
+                                </div>
+
+                                <button type="submit" class="btn btn-primary">Log in</button>
+
+                                <?php
                                 }
                                 else
                                 {
-                                    $LDAPresult=0;
+                                    $LDAPconn=@ldap_connect(MONASH_DIR);
+                                    if($LDAPconn)
+                                    {
+                                        $LDAPsearch=@ldap_search($LDAPconn,MONASH_FILTER,
+                                            "uid=".$_POST["uname"]);
+                                        if($LDAPsearch)
+                                        {
+                                            $LDAPinfo =
+                                                @ldap_first_entry($LDAPconn,$LDAPsearch);
+                                            if($LDAPinfo)
+                                            {
+                                                $LDAPresult=
+                                                    @ldap_bind($LDAPconn,
+                                                        ldap_get_dn($LDAPconn, $LDAPinfo),
+                                                        $_POST["pword"]);
+                                            }
+                                            else
+                                            {
+                                                $LDAPresult=0;
+                                            }
+                                        }
+                                        else
+                                        {
+                                            $LDAPresult=0;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        $LDAPresult=0;
+                                    }
+                                    if($LDAPresult)
+                                    {
+                                        //This is where the action can become true
+                                        echo "Valid User";
+                                        header("Location: ../main-page/Home.php");
+                                    }
+                                    else
+                                    {
+                                        //This is where the error message should sit
+                                        echo "Invalid User";
+                                        ?>
+                                            <div class="alert alert-danger" role="alert">
+                                                <strong>Oh snap!</strong> Change a few things up and try submitting again.
+                                            </div>
+                                        <?php
+                                    }
                                 }
-                            }
-                            else
-                            {
-                                $LDAPresult=0;
-                            }
-                        }
-                        else
-                        {
-                            $LDAPresult=0;
-                        }
-                        if($LDAPresult)
-                        {
-                            $_SESSION["login"] = true;
-                            header("Location: ../main-page/Home.php");
-                        }
-                        else
-                        {
-                            $_SESSION["login"] = false;
-                            header("Location: Login.php");
-                        }
-                    }
-                    ?>
+                                ?>
 
+                            </form>
+                        </div>
+                    </div>
                 </div>
 
             </div>
         </div>
 
         <!-- Add a footer to each displayed page -->
+        <!-- Display a footer -->
         <div class="col-md-12" >
             <nav class="navbar navbar-fixed-bottom navbar-light bg-faded" id="footer">
                 <div class="col-md-2 offset-md-8">
                     <p>Click to display code:</p>
                 </div>
                 <div class="col-md-2">
-                    <a class="btn btn-primary display-code" href="../DisplayCode.php" role="button" target="_blank">Login</a>
+                    <a class="btn btn-primary display-code" href="../DisplayCode.php" role="button" target="_blank">Property</a>
                 </div>
             </nav>
         </div>
     </div>
+
+
 </div>
+
 
 <!-- jQuery first, then Tether, then Bootstrap JS. -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js" integrity="sha384-THPy051/pYDQGanwU6poAc/hOdQxjnOEXzbT+OuUAFqNqFjL+4IGLBgCJC3ZOShY" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.2.0/js/tether.min.js" integrity="sha384-Plbmg8JY28KFelvJVai01l8WyZzrYWG825m+cZ0eDDS1f7d/js6ikvy1+X+guPIB" crossorigin="anonymous"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.4/js/bootstrap.min.js" integrity="sha384-VjEeINv9OSwtWFLAtmc4JCtEJXXBub00gtSnszmspDLCtC0I4z4nqz7rEFbIZLLU" crossorigin="anonymous"></script>
-
 </body>
 </html>
-<?php
-ob_end_flush();
-?>
+
