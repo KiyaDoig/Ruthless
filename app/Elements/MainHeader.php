e95cea2db62aba36dd552c4ecc3b0afe1d6ed004
KiyaDoig <kiya.doig@gmail.com>
2016-10-06 21:08:43 +1100

    Add if to logout

--- app/Elements/MainHeader.php
+++ app/Elements/MainHeader.php
@@ -1,7 +1,17 @@
 <!-- Main header element -->
 <div class="col-md-12" id="main-header-right">
     <div class="col-md-10"></div>
-    <div class="col-md-2">
-        <button type="button" class="btn btn-link" id="logout" onclick="location.href='../login-page/logout.php';">Logout</button>
-    </div>
+    <?php
+    if (isset($_SESSION["login"]))
+    {
+        if($_SESSION["login"] == true)
+        {
+        ?>
+           <div class="col-md-2">
+        		<button type="button" class="btn btn-link" id="logout" onclick="location.href='../login-page/logout.php';">Logout</button>
+    		</div>
+    	<?php
+        }
+    }
+    ?>
 </div>
